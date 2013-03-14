bbe48fe1e220c6853684b4de8bd65ec25e0ff023
Florian Amstutz <florian@amstutz.nu>
2013-03-14 20:32:52 +0100

    Finalized ContactService.

--- SoCrm.Services.Contacts.Provider/ContactService.svc.cs
+++ SoCrm.Services.Contacts.Provider/ContactService.svc.cs
@@ -2,44 +2,55 @@
 {
     using System;
     using System.Collections.Generic;
+    using System.Linq;
 
     using SoCrm.Services.Contacts.Contracts;
+    using SoCrm.Services.Customers.Contracts;
+    using SoCrm.Services.Security.Contracts;
 
     public class ContactService : IContactService
     {
+        private ContactPersistence.PersistenceServiceOf_ContactClient client;
+
+        public ContactService()
+        {
+            this.client = new ContactPersistence.PersistenceServiceOf_ContactClient();
+        }
+
         public IEnumerable<Contact> GetContactsByPerson(Person person)
         {
-            throw new NotImplementedException();
+            return this.client.GetAll().Where(c => c.Person == person);
         }
 
         public IEnumerable<Contact> GetContactsByCompany(Company company)
         {
-            throw new NotImplementedException();
+            return this.client.GetAll().Where(c => c.Person.Employer == company);
         }
 
         public IEnumerable<Contact> GetContactsByUser(User user)
         {
-            throw new NotImplementedException();
+            return this.client.GetAll().Where(c => c.User == user);
         }
 
         public Contact GetContactByObjectId(Guid objectId)
         {
-            throw new NotImplementedException();
+            return this.client.Get(objectId);
         }
 
         public IEnumerable<Contact> GetContactsByContactMedium(ContactMedium contactMedium)
         {
-            throw new NotImplementedException();
+            return this.client.GetAll().Where(c => c.Medium == contactMedium);
         }
 
         public IEnumerable<ContactMedium> GetAllContactMediums()
         {
-            throw new NotImplementedException();
+            return Enum.GetValues(typeof(ContactMedium)).Cast<ContactMedium>();
         }
 
         public void CreateContact(User user, Person person, string content, ContactMedium medium, DateTime dateTime)
         {
-            throw new NotImplementedException();
+            this.client.Save(
+                new Contact() { Content = content, DateTime = dateTime, Medium = medium, Person = person, User = user });
         }
     }
 }
