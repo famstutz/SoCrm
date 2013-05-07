541b724cd09f899a9392061ca99498c91a167b60
Florian Amstutz <florian@amstutz.nu>
2013-05-07 19:15:35 +0200

    Finalized contracts unit tests

---
+++ Tests/SoCrm.Services.Logging.Contracts.Tests/LogEventTests.cs
@@ -0,0 +1,60 @@
+﻿namespace SoCrm.Services.Logging.Contracts.Tests
+{
+    using System;
+
+    using NUnit.Framework;
+
+    using SoCrm.Tests.Common;
+
+    [TestFixture]
+    public class LogEventTests
+    {
+        [Test]
+        public void Constructor()
+        {
+            var logEvent = new LogEvent();
+
+            Assert.IsNotNull(logEvent);
+        }
+
+        [Test]
+        public void PropertiesDefaultValues()
+        {
+            var logEvent = new LogEvent();
+
+            AssertExtensions.IsDefault(logEvent.Message);
+            AssertExtensions.IsDefault(logEvent.Severity);
+            AssertExtensions.IsDefault(logEvent.TimeStamp);
+        }
+
+        [Test]
+        public void SetMessage()
+        {
+            var logEvent = new LogEvent();
+            var message = "Test 1, 2, 3";
+            logEvent.Message = message;
+
+            Assert.AreEqual(message, logEvent.Message);
+        }
+
+        [Test]
+        public void SetSeverity()
+        {
+            var logEvent = new LogEvent();
+            var severity = Severity.Warning;
+            logEvent.Severity = severity;
+
+            Assert.AreEqual(severity, logEvent.Severity);
+        }
+
+        [Test]
+        public void SetTimeStamp()
+        {
+            var logEvent = new LogEvent();
+            var timeStamp = DateTime.Now;
+            logEvent.TimeStamp = timeStamp;
+
+            Assert.AreEqual(timeStamp, logEvent.TimeStamp);
+        }
+    }
+}
---
+++ Tests/SoCrm.Services.Security.Contracts.Tests/Class1.cs
@@ -0,0 +1,58 @@
+﻿namespace SoCrm.Services.Security.Contracts.Tests
+{
+    using NUnit.Framework;
+
+    using SoCrm.Tests.Common;
+
+    [TestFixture]
+    public class UserTests
+    {
+        [Test]
+        public void Constructor()
+        {
+            var user = new User();
+
+            Assert.IsNotNull(user);
+        }
+
+        [Test]
+        public void PropertiesDefaultValues()
+        {
+            var user = new User();
+
+            AssertExtensions.IsDefault(user.UserName);
+            AssertExtensions.IsDefault(user.Role);
+            AssertExtensions.IsDefault(user.Password);
+        }
+
+        [Test]
+        public void SetUserName()
+        {
+            var user = new User();
+            var userName = "Hans";
+            user.UserName = userName;
+
+            Assert.AreEqual(userName, user.UserName);
+        }
+
+        [Test]
+        public void SetRole()
+        {
+            var user = new User();
+            var role = Role.Administrator;
+            user.Role = role;
+
+            Assert.AreEqual(role, user.Role);
+        }
+
+        [Test]
+        public void SetPassword()
+        {
+            var user = new User();
+            var password = "SecretPassword";
+            user.Password = password;
+
+            Assert.AreEqual(password, user.Password);
+        }
+    }
+}
