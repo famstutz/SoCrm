58906225747ef757eaf695d8f22fa12e444a289f
Florian Amstutz <florian@amstutz.nu>
2013-06-04 19:07:22 +0200

    Added acceptance tests

---
+++ Tests/SoCrm.Tests.AcceptanceTests/Uc1Test.cs
@@ -0,0 +1,47 @@
+﻿namespace SoCrm.Tests.AcceptanceTests
+{
+    using System.IO;
+
+    using Microsoft.VisualStudio.TestTools.UITesting;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [CodedUITest]
+    public class Uc1Test
+    {
+        private UIMap map;
+        private ApplicationUnderTest app;
+
+        public UIMap UIMap
+        {
+            get
+            {
+                if ((this.map == null))
+                {
+                    this.map = new UIMap();
+                }
+
+                return this.map;
+            }
+        }
+
+        [TestInitialize]
+        public void MyTestInitialize()
+        {
+            this.app = ApplicationUnderTest.Launch(Path.Combine(Directory.GetCurrentDirectory(), @"App\SoCrm.Presentation.App.exe"));
+        }
+
+        [TestCleanup]
+        public void MyTestCleanup()
+        {
+            this.app.Close();
+        }
+
+        [TestMethod]
+        public void RunUc1Test()
+        {
+            this.UIMap.OpenAuthentication();
+            this.UIMap.EnterCredentials();
+            this.UIMap.LogOn();
+        }
+    }
+}
